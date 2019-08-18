1eb6cc4e7b7a05647d8187ccd8f545920a1086f2
Adrian Eib <adrian.eib@showmax.com>
2019-08-18 20:49:36 +0200

    adding comments & clean up

--- app/application.c
+++ app/application.c
@@ -1,74 +1,73 @@
 #include <application.h>
 
+// define subcribe topic for MQTT
 // subscribe table, format: topic, expect payload type, callback, user param
 static const bc_radio_sub_t subs[] = {
     // MQTT subscribe topic which will trigger pump
     { "task/pump/start/5", BC_RADIO_SUB_PT_INT, exec_tasks, NULL }
 };
 
-// Core module LED instance
+// core module LED instance
 bc_led_t led;
-// Core module button instance
+// core module button instance
 bc_button_t button;
-// Core module temperature sensor instance
+// core module temperature sensor instance
 bc_tmp112_t tmp112;
 
-
-
 #if MODULE_SENSOR
+    // define instances for BigClown soil sensors
     // BigClown Soil sensor instance
     bc_soil_sensor_t soil_sensor;
-    // Sensors array
+    // sensors array
     bc_soil_sensor_sensor_t sensors[MAX_SOIL_SENSORS];
-    // Time of next sensor temperature report
+    // time of next sensor temperature report
     bc_tick_t sensor_temperature_tick_report[MAX_SOIL_SENSORS];
-    // Last sensor temperature value used for change comparison
+    // last sensor temperature value used for change comparison
     float sensor_last_published_temperature[MAX_SOIL_SENSORS];
-    // Time of next sensor moisture report
+    // time of next sensor moisture report
     bc_tick_t sensor_moisture_tick_report[MAX_SOIL_SENSORS];
-    // Last sensor moisture value used for change comparison
+    // last sensor moisture value used for change comparison
     int sensor_last_published_moisture[MAX_SOIL_SENSORS];
 #endif
 
 void application_init(void)
 {
-    // Initialize logging
+    // initialize logging
     bc_log_init(BC_LOG_LEVEL_DUMP, BC_LOG_TIMESTAMP_ABS);
 
-    // Initialize LED
+    // initialize a LED
     bc_led_init(&led, BC_GPIO_LED, false, false);
     bc_led_set_mode(&led, BC_LED_MODE_OFF);
 
-    // Initialize button
+    // initialize a button
     bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
     bc_button_set_event_handler(&button, button_event_handler, NULL);
 
-    // Initialize radio
+    // initialize radio - sleeping mode is suitable for battery
     bc_radio_init(BC_RADIO_MODE_NODE_SLEEPING);
-    //bc_radio_init(BC_RADIO_MODE_NODE_LISTENING);
     bc_radio_pairing_request(FIRMWARE, VERSION);
-    // subscribe to topic
+    // subscribe to MQTT topic
     bc_radio_set_subs((bc_radio_sub_t *) subs, sizeof(subs)/sizeof(bc_radio_sub_t));
-    bc_radio_set_rx_timeout_for_sleeping_node(50);
+    bc_radio_set_rx_timeout_for_sleeping_node(RADIO_RX_TIMEOUT);
 
-    // Initialize battery module
+    // initialize a battery module
     bc_module_battery_init();
     bc_module_battery_set_event_handler(battery_event_handler, NULL);
     bc_module_battery_set_update_interval(BATTERY_UPDATE_INTERVAL);
 
-    // Initialize temperature sensor
+    // initialize a temperature sensor
     bc_tmp112_init(&tmp112, BC_I2C_I2C0, 0x49);
     bc_tmp112_set_event_handler(&tmp112, tmp112_event_handler, NULL);
     // set measure inteval
-    bc_tmp112_set_update_interval(&tmp112, TEMPERATURE_UPDATE_SERVICE_INTERVAL);
+    bc_tmp112_set_update_interval(&tmp112, SENSOR_UPDATE_SERVICE_INTERVAL);
 
-    // initialize P0 for a transistor/switch to turn on water pump
+    // initialize port for a transistor/switch to turn on a water pump
     bc_gpio_init(WATER_PUMP_POWER_ID);
     bc_gpio_set_mode(WATER_PUMP_POWER_ID, BC_GPIO_MODE_OUTPUT);
     // create tasks - stop water pump
     tasks._stop_water_pump_task_id = bc_scheduler_register(_stop_water_pump, NULL, 100);
 
-    // initialize senzor for water float
+    // initialize ports for water floats
     bc_gpio_init(water_float_ports.low);
     bc_gpio_init(water_float_ports.high);
     bc_gpio_set_mode(water_float_ports.low, BC_GPIO_MODE_INPUT);
@@ -80,25 +79,37 @@ void application_init(void)
     tasks._measure_water_level_task_id = bc_scheduler_register(_measure_water_level, NULL, 1000);
 
 #if MODULE_SENSOR
-    // Initialize BigClown soil sensor
-    bc_soil_sensor_init_multiple(&soil_sensor, sensors, 5);
+    // initialize BigClown soil sensor
+    bc_soil_sensor_init_multiple(&soil_sensor, sensors, SOIL_MOISTURE_SENSOR_COUNT);
     bc_soil_sensor_set_event_handler(&soil_sensor, soil_sensor_event_handler, NULL);
     bc_soil_sensor_set_update_interval(&soil_sensor, SENSOR_UPDATE_SERVICE_INTERVAL);
-    bc_log_debug("Number of soil sensors: %d", bc_soil_sensor_get_sensor_found(&soil_sensor));
+    // switch BC soil sensor to normal mode
+    bc_scheduler_register(switch_to_normal_mode_bc_soil_sensor_task, NULL, SERVICE_MODE_INTERVAL);
 #else
-    // initialize power for soil sensor
+    // initialize power for a soil sensor
     bc_gpio_init(SOIL_MOISTURE_POWER_ID);
     bc_gpio_set_mode(SOIL_MOISTURE_POWER_ID, BC_GPIO_MODE_OUTPUT);
-    // initialization of analog sensors for reading values from moisture sensor
+    // initialization of analog port for reading values from a soil moisture sensor
     bc_adc_init();
-    // measure soil moisture
+    // set a first moisture value
     values.moisture = 0;
-    tasks._measure_moisture_task_id = bc_scheduler_register(_measure_moisture_task, NULL, 1000);
-    tasks._stop_measuring_moisture_task_id = bc_scheduler_register(_stop_measuring_moisture_task, NULL, 1000);
+    // create task for regular measurements of moisture
+    tasks._measure_moisture_id = bc_scheduler_register(_measure_moisture_task, NULL, 1000);
+    // create task to stop moisture measurement
+    tasks._stop_measuring_moisture_task_id = bc_scheduler_register(_stop_measuring_moisture, NULL, 1000);
 #endif
-    bc_log_debug("Detected battery format: %d", bc_module_battery_get_format());
+    // switch to normal mode for temperature sensor on core module
+    bc_scheduler_register(switch_to_normal_mode_task, NULL, SERVICE_MODE_INTERVAL);
 }
 
+// switch from service mode to normal mode
+void switch_to_normal_mode_task(void *param)
+{
+    bc_tmp112_set_update_interval(&tmp112, SENSOR_UPDATE_NORMAL_INTERVAL);
+    bc_scheduler_unregister(bc_scheduler_get_current_task_id());
+}
+
+// event handler for a core module's button
 void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
 {
     (void) self;
@@ -107,22 +118,18 @@ void button_event_handler(bc_button_t *self, bc_button_event_t event, void *even
     if (event == BC_BUTTON_EVENT_PRESS)
     {
         bc_led_pulse(&led, 100);
-
         static uint16_t event_count = 0;
-
         bc_radio_pub_push_button(&event_count);
-
         event_count++;
     }
 }
 
+// event handler for a battery module
 void battery_event_handler(bc_module_battery_event_t event, void *event_param)
 {
     (void) event;
     (void) event_param;
-
     float voltage;
-    //int percentage;
 
     if (bc_module_battery_get_voltage(&voltage))
     {
@@ -131,7 +138,7 @@ void battery_event_handler(bc_module_battery_event_t event, void *event_param)
     }
 }
 
-// temperature event handler
+// event handler for a temperature provided by core module
 void tmp112_event_handler(bc_tmp112_t *self, bc_tmp112_event_t event, void *event_param)
 {
     // Time of next report
@@ -155,9 +162,8 @@ void tmp112_event_handler(bc_tmp112_t *self, bc_tmp112_event_t event, void *even
                 // Publish message on radio
                 publish = true;
             }
-
             // Is temperature difference from last published value significant?
-            if (fabsf(temperature - last_published_temperature) >= TEMPERATURE_PUB_DIFFERENCE)
+            else if (fabsf(temperature - last_published_temperature) >= TEMPERATURE_PUB_DIFFERENCE)
             {
                 // Publish message on radio
                 publish = true;
@@ -186,17 +192,20 @@ void tmp112_event_handler(bc_tmp112_t *self, bc_tmp112_event_t event, void *even
     }
 }
 
-// Tasks
+// start a water pump
 void _start_water_pump(int pump_runtime) {
     bc_led_pulse(&led, LED_LIGHT_DUR);
     bc_gpio_set_output(WATER_PUMP_POWER_ID, 1);
     bc_scheduler_plan_relative(tasks._stop_water_pump_task_id, pump_runtime);
 }
 
+// stop a water pump
 void _stop_water_pump() {
     bc_gpio_set_output(WATER_PUMP_POWER_ID, 0);
 }
 
+// measure level of water with float sensors and published their states
+// in case of a change
 void _measure_water_level() {
     int low = bc_gpio_get_input(water_float_ports.low);
     int high = bc_gpio_get_input(water_float_ports.high);
@@ -213,6 +222,9 @@ void _measure_water_level() {
     bc_scheduler_plan_relative(tasks._measure_water_level_task_id, WATER_FLOAT_DELAY);
 }
 
+// it is executed based on a request from MQTT
+// it does run a water pump and check water level
+// client can define how long a pump should run - see a value of request's payload
 void exec_tasks(uint64_t *id, const char *topic, void *value, void *param)
 {
     // use payload requested by client
@@ -224,16 +236,17 @@ void exec_tasks(uint64_t *id, const char *topic, void *value, void *param)
             "(> default_value * 10). Setting default value: %i", PUMP_RUNTIME);
         pump_runtime = PUMP_RUNTIME;
     }
-    bc_log_debug("Pump runtime final %i", pump_runtime);
-    bc_log_debug("starting tasks");
-    bc_radio_pub_string("water/-/pump/state", "on");
+    bc_log_debug("Pump runtime: %i", pump_runtime);
+    bc_log_debug("starting a water pump");
     _start_water_pump(pump_runtime);
-    bc_log_debug("tasks done");
-    bc_radio_pub_string("water/-/pump/state", "off");
+    bc_log_debug("watering finished");
+    // measure water level after watering
     bc_scheduler_plan_now(tasks._measure_water_level_task_id);
 }
 
 #if MODULE_SENSOR
+    // event handler for BigClown soil moisture sensor
+    // it reads&publishes moisture&temperature provided by soil moisture sensor
     void soil_sensor_event_handler(bc_soil_sensor_t *self, uint64_t device_address, bc_soil_sensor_event_t event, void *event_param)
     {
         static char topic[64];
@@ -259,49 +272,53 @@ void exec_tasks(uint64_t *id, const char *topic, void *value, void *param)
                 if (bc_tick_get() >= sensor_temperature_tick_report[index])
                 {
                     // Publish message on radio
+                    bc_log_debug("it's time to publish temperature due to defined interval");
                     publish = true;
                 }
-
                 // Is temperature difference from last published value significant?
-                if (fabsf(temperature - sensor_last_published_temperature[index]) >= TEMPERATURE_PUB_DIFFERENCE)
+                else if (fabsf(temperature - sensor_last_published_temperature[index]) >= TEMPERATURE_PUB_DIFFERENCE)
                 {
                     // Publish message on radio
+                    bc_log_debug("it's time to publish temperature as difference is significant: %f", fabsf(temperature - sensor_last_published_temperature[index]));
                     publish = true;
                 }
 
                 if (publish)
                 {
                     snprintf(topic, sizeof(topic), "soil-sensor/%llx/temperature", device_address);
-
                     // Publish temperature message on radio
                     bc_radio_pub_float(topic, &temperature);
-
                     // Schedule next temperature report
                     sensor_temperature_tick_report[index] = bc_tick_get() + TEMPERATURE_PUB_INTERVAL;
-
                     // Remember last published value
                     sensor_last_published_temperature[index] = temperature;
                 }
             }
 
-            int moisture;
+            // going to fetch moisutre as raw capacitance
+            // measured moisture can be retrieved in percentage but it's experimental
+            // and it depends on sensor calibration
+            int raw_cap;
+            uint16_t raw_cap_u16;
 
-            if (bc_soil_sensor_get_moisture(self, device_address, &moisture))
+            if (bc_soil_sensor_get_cap_raw(self, device_address, &raw_cap_u16))
             {
                 bool publish = false;
-                bc_log_debug("Soil moisture for sensor ID %llx: %d", device_address, moisture);
+                raw_cap = (int)raw_cap_u16;
+                bc_log_debug("Soil moisture for sensor ID %llx: %u", device_address, raw_cap_u16);
 
                 // Is time up to report sensor moisture?
                 if (bc_tick_get() >= sensor_moisture_tick_report[index])
                 {
                     // Publish message on radio
+                    bc_log_debug("it's time to publish capacitance due to defined interval");
                     publish = true;
                 }
-
-                // Is sensor moisture difference from last published value significant?
-                if (abs(moisture - sensor_last_published_moisture[index]) >= SENSOR_MOISTURE_PUB_DIFFERENCE)
+                // Is sensor capacitance difference from last published value significant?
+                else if (abs(raw_cap - sensor_last_published_moisture[index]) >= SENSOR_MOISTURE_PUB_DIFFERENCE)
                 {
                     // Publish message on radio
+                    bc_log_debug("it's time to publish capacitance as difference is significant: %d", abs(raw_cap - sensor_last_published_moisture[index]));
                     publish = true;
                 }
 
@@ -310,22 +327,13 @@ void exec_tasks(uint64_t *id, const char *topic, void *value, void *param)
                     snprintf(topic, sizeof(topic), "soil-sensor/%llx/moisture", device_address);
 
                     // Publish sensor moisture message on radio
-                    bc_radio_pub_int(topic, &moisture);
-
-                    /*
-                    // Print also RAW value from capacitance chip
-                    int raw = 0;
-                    bc_soil_sensor_get_cap_raw(self, device_address, (uint16_t*)&raw);
-                    snprintf(topic, sizeof(topic), "soil-sensor/%llx/raw", device_address);
-                    // Publish sensor RAW message on radio
-                    bc_radio_pub_int(topic, &raw);
-                    */
+                    bc_radio_pub_int(topic, &raw_cap);
 
                     // Schedule next moisture report
-                    sensor_moisture_tick_report[index] = bc_tick_get() + MEASURE_MOISTURE_DELAY;
+                    sensor_moisture_tick_report[index] = bc_tick_get() + MEASURE_MOISTURE_INTERVAL;
 
                     // Remember last published value
-                    sensor_last_published_moisture[index] = moisture;
+                    sensor_last_published_moisture[index] = raw_cap;
                 }
             }
         }
@@ -335,19 +343,32 @@ void exec_tasks(uint64_t *id, const char *topic, void *value, void *param)
             bc_radio_pub_int("soil-sensor/-/error", &error);
         }
     }
+
+    // switch from service mode to normal mode for BC soil moisture sensor
+    void switch_to_normal_mode_bc_soil_sensor_task(void *param)
+    {
+        bc_soil_sensor_set_update_interval(&soil_sensor, SENSOR_UPDATE_NORMAL_INTERVAL);
+        bc_scheduler_unregister(bc_scheduler_get_current_task_id());
+    }
 #else
-    void _measure_moisture_task() {
+    // Functions related to non BigClown moisture sensor
+    // measure moisture and schedule jobs:
+    // - to stop a measurement
+    // - run next a measurement
+    void _measure_moisture() {
         bc_gpio_set_output(SOIL_MOISTURE_POWER_ID, 1);
-        bc_scheduler_plan_relative(tasks._stop_measuring_moisture_task_id, 25);
-        bc_scheduler_plan_relative(tasks._measure_moisture_task_id, MEASURE_MOISTURE_DELAY);
+        bc_scheduler_plan_relative(tasks._stop_measuring_moisture_task_id, SOIL_MOISTURE_POWER_ON_INTERVAL);
+        bc_scheduler_plan_relative(tasks._measure_moisture_id, MEASURE_MOISTURE_INTERVAL);
     }
 
-    void _stop_measuring_moisture_task() {
+    // get mositure, publish moisture to MQTT, stop moisture measurement
+    void _stop_measuring_moisture() {
         static char topic[64];
         snprintf(topic, sizeof(topic), "soil-sensor/%d/moisture", SOIL_MOISTURE_PORT_ID);
         bc_log_debug("Publish topic: %s", topic);
         bc_adc_get_value(SOIL_MOISTURE_PORT_ID, &values.moisture);
         int moisture = values.moisture;
+        // always publish moisture as it's never the same value
         bc_radio_pub_int(topic, &moisture);
         // power off port for a soil sensor
         bc_gpio_set_output(SOIL_MOISTURE_POWER_ID, 0);
--- app/application.h
+++ app/application.h
@@ -11,74 +11,79 @@
 
 #include <bcl.h>
 
-#define MODULE_SENSOR true
-
+// set to true when you will use sensor module&BigClown soil moisture sensor
+#define MODULE_SENSOR                           true
+// set how long will a node listen for MQTT reply after publishing a data (ms)
+#define RADIO_RX_TIMEOUT                        50
+// set how long the APP will run in sevice mode (ms)
+#define SERVICE_MODE_INTERVAL                   (5 * 60 * 1000)
+// how often will be checked battery for update (ms)
+#define BATTERY_UPDATE_INTERVAL                 (60 * 60 * 1000)
 // how often will be moisture measured (ms)
-#define MEASURE_MOISTURE_DELAY              (30 * 1000)
+#define MEASURE_MOISTURE_INTERVAL               (15 * 60 * 1000)
 // how often will be water float checked (ms)
-#define WATER_FLOAT_DELAY                   (10 * 60 * 1000)
+#define WATER_FLOAT_DELAY                       (15 * 60 * 1000)
 // how long will led lighting
-#define LED_LIGHT_DUR                       200
+#define LED_LIGHT_DUR                           200
 // define default time how long (in miliseconds) pump should run
 // a value can be overriden by client
-#define PUMP_RUNTIME                        (3 * 1000)
+#define PUMP_RUNTIME                            (3 * 1000)
 // how often will be temperature published (ms)
-#define TEMPERATURE_PUB_INTERVAL            (15 * 60 * 1000)
+#define TEMPERATURE_PUB_INTERVAL                (15 * 60 * 1000)
 // threshold which define difference in temperature
 // if is threshold exceeded temperature will be published
-#define TEMPERATURE_PUB_DIFFERENCE          1.0f
-// how often will be temperature measured (ms)
-#define TEMPERATURE_UPDATE_SERVICE_INTERVAL (10 * 60 * 1000)
+#define TEMPERATURE_PUB_DIFFERENCE              1.0f
+// how often will be temperature, moisture, etc measured in service mode (ms)
+#define SENSOR_UPDATE_SERVICE_INTERVAL          (60 * 1000)
+// how often will be temperature, moisture, etc. measured in normal mode (ms)
+#define SENSOR_UPDATE_NORMAL_INTERVAL           (10 * 60 * 1000)
 // Set variable for water pump GPIO pin
-#define WATER_PUMP_POWER_ID BC_GPIO_P7
-// Set variable for water float sensors for low/high water level
-#define WATER_FLOAT_LOW_POWER_ID BC_GPIO_P8
-#define WATER_FLOAT_HIGH_POWER_ID BC_GPIO_P9
+#define WATER_PUMP_POWER_ID                     BC_GPIO_P7
+// Set variables for water float sensors for low/high water level
+#define WATER_FLOAT_LOW_POWER_ID                BC_GPIO_P8
+#define WATER_FLOAT_HIGH_POWER_ID               BC_GPIO_P9
 #if MODULE_SENSOR
     // BigClown soil sensor macros
-    #define MAX_SOIL_SENSORS 5
-    #define SENSOR_UPDATE_SERVICE_INTERVAL      (30 * 1000)
-    //#define SENSOR_UPDATE_NORMAL_INTERVAL       (60 * 1000)
-    //#define SENSOR_MOISTURE_PUB_INTERVAL        (5 * 60 * 1000)
+    #define MAX_SOIL_SENSORS                    5
+    #define SOIL_MOISTURE_SENSOR_COUNT          1
     // threshold which define difference in moisture
     // if is threshold exceeded moisture will be published
-    #define SENSOR_MOISTURE_PUB_DIFFERENCE      5.0f
+    #define SENSOR_MOISTURE_PUB_DIFFERENCE      1
     // function definition for BigClown soil sensor
     void soil_sensor_event_handler(bc_soil_sensor_t *self, uint64_t device_address, bc_soil_sensor_event_t event, void *event_param);
+    void switch_to_normal_mode_bc_soil_sensor_task(void *param);
 #else
-    // Set macros for moisture soil sensor
-    #define SOIL_MOISTURE_PORT_ID BC_ADC_CHANNEL_A5
-    #define SOIL_MOISTURE_POWER_ID BC_GPIO_P6
+    // set ports for power&analog connections to a soil moisture sensor
+    #define SOIL_MOISTURE_PORT_ID               BC_ADC_CHANNEL_A5
+    #define SOIL_MOISTURE_POWER_ID              BC_GPIO_P6
+    #define SOIL_MOISTURE_POWER_ON_INTERVAL     25
     // function definition
-    void _measure_moisture_task();
-    void _stop_measuring_moisture_task();
+    void _measure_moisture();
+    void _stop_measuring_moisture();
 #endif
-//#if BATTERY_MINI
-//    #define BC_MODULE_BATTERY_FORMAT BC_MODULE_BATTERY_FORMAT_MINI
-//#else
-//    #define BC_MODULE_BATTERY_FORMAT BC_MODULE_BATTERY_FORMAT_STANDARD
-//#endif
-// how often will be battery voltage measured (ms)
-#define BATTERY_UPDATE_INTERVAL             (60 * 60 * 1000)
 
+// structre for all tasks
 struct {
-    bc_scheduler_task_id_t _measure_moisture_task_id;
+    bc_scheduler_task_id_t _measure_moisture_id;
     bc_scheduler_task_id_t _stop_measuring_moisture_task_id;
     bc_scheduler_task_id_t _start_water_pump_task_id;
     bc_scheduler_task_id_t _stop_water_pump_task_id;
     bc_scheduler_task_id_t _measure_water_level_task_id;
 } tasks;
 
+// structure for water float ports
 struct {
     int low;
     int high;
 } water_float_ports = { WATER_FLOAT_LOW_POWER_ID, WATER_FLOAT_HIGH_POWER_ID };
 
+// structure for values of water float ports
 struct {
     int low;
     int high;
 } water_float_states;
 
+// structure for all measured values
 struct {
     float_t battery_voltage;
     float_t battery_pct;
@@ -86,12 +91,13 @@ struct {
 } values;
 
 // function definition
+void _measure_water_level();
 void _start_water_pump();
 void _stop_water_pump();
-void _measure_water_level();
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param);
 void battery_event_handler(bc_module_battery_event_t event, void *event_param);
-void tmp112_event_handler(bc_tmp112_t *self, bc_tmp112_event_t event, void *event_param);
+void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param);
 void exec_tasks(uint64_t *id, const char *topic, void *value, void *param);
+void switch_to_normal_mode_task(void *param);
+void tmp112_event_handler(bc_tmp112_t *self, bc_tmp112_event_t event, void *event_param);
 
 #endif // _APPLICATION_H
